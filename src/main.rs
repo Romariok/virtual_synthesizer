421c88779d573a60df22ea94999a1c90d1d5bf32
Romariok <ronya-ko@yandex.ru>
2023-10-16 21:07:23 +0300

    try to make multithreading for playing sounds

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 mod data;
 mod services;
-
+extern crate rodio;
 
 use data::collection::BasicPiano;
 use winit::{
-    event::{Event, VirtualKeyCode, ElementState, KeyboardInput},
+    event::{Event, VirtualKeyCode, ElementState},
     event_loop::{EventLoop, ControlFlow},
     window::WindowBuilder,
 };
-use std::fs::File;
+use std::{fs::File, sync::Arc};
 use std::io::BufReader;
-use std::thread::sleep;
-use std::time::Duration;
 
-use rodio::source::Source;
+use std::time::Duration;
+use std::vec::Vec;
+use std::thread;
+use std::collections::HashMap;
+
 use rodio::OutputStream;
 use rodio::OutputStreamHandle;
 
+extern crate hound;
 
-extern crate hound; // Библиотека для записи аудиофайлов
-
-// implementation Display for VirtualKeyCode
 fn virtual_keycode_to_string(key_code: VirtualKeyCode) -> String {
     match key_code {
         VirtualKeyCode::A => "A".to_string(),
@@ -34,45 +34,55 @@ fn virtual_keycode_to_string(key_code: VirtualKeyCode) -> String {
     }
 }
 
-fn play_sound(keycode: VirtualKeyCode) {
-    let file = File::open("resources/basic/".to_string()+&virtual_keycode_to_string(keycode)+".wav").expect("Failed to open file");
+fn play_wav_file(file_name: &str, handle: OutputStreamHandle) {
+    let file_path = format!("resources/basic/{}", file_name);
+    let file = File::open(&file_path).expect("Failed to open file");
     let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-
-    let (stream, handle) = OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
-
     sink.append(source);
-
-    sleep(Duration::from_secs(1));  
+    println!("Playing {:?}", file_name);
+    thread::sleep(Duration::from_secs(1));
     sink.stop();
 }
 
 fn main() {
+    let (stream, handle) = OutputStream::try_default().unwrap();
     let spec = hound::WavSpec {
         channels: 1,
         sample_rate: 44100,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
-// Incilization of sounds for piano
+    let mut wav_files: Vec<String> = Vec::new();
+
+    // Create a basic piano
     {
         let basic: BasicPiano = BasicPiano::initBasic();
-        // let note_basic: Vec<Vec<i16>> = Vec::from([basic.A, basic.B, basic.C, basic.D, basic.E, basic.F, basic.G]);
-
 
         for (key, value) in basic.as_map() {
-            let mut writer = hound::WavWriter::
-            create("resources/basic/".to_string()+key+".wav", spec).unwrap();
-            for sample in value{
+            let mut writer = hound::WavWriter::create(format!("resources/basic/{}.wav", key), spec).unwrap();
+            wav_files.push(format!("resources/basic/{}.wav", key));
+
+            for sample in value {
                 writer.write_sample(sample).unwrap();
             }
-
         }
     }
 
-// Infinite loop of tracking keyboard events
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut threads: HashMap<String, thread::JoinHandle<()>> = HashMap::new();
+
+    // Spawn a new thread for each file
+    for file_name in &wav_files {
+        let file_name_clone = file_name.to_string();
+        let handle_clone = handle.clone();
+        let thread = thread::spawn(move || {
+            play_wav_file(&file_name_clone, handle_clone);
+        });
+
+        threads.insert(file_name.to_string(), thread);
+    }
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -82,19 +92,21 @@ fn main() {
                 event,
                 window_id,
             } => match event {
-                // Handle keyboard input events
                 winit::event::WindowEvent::KeyboardInput { input, .. } => {
                     if input.state == ElementState::Pressed {
                         if let Some(keycode) = input.virtual_keycode {
                             println!("Key pressed: {:?}", keycode);
                             match keycode {
-                                VirtualKeyCode::C  => play_sound(keycode),
-                                VirtualKeyCode::D  => play_sound(keycode),
-                                VirtualKeyCode::E  => play_sound(keycode),
-                                VirtualKeyCode::F  => play_sound(keycode),
-                                VirtualKeyCode::G  => play_sound(keycode),
-                                VirtualKeyCode::A  => play_sound(keycode),
-                                VirtualKeyCode::B  => play_sound(keycode),
+                                VirtualKeyCode::C | VirtualKeyCode::D | VirtualKeyCode::E
+                                | VirtualKeyCode::F | VirtualKeyCode::G | VirtualKeyCode::A
+                                | VirtualKeyCode::B => {
+                                    let path = format!("{}.wav", virtual_keycode_to_string(keycode));
+                                    if let Some(thread_handle) = threads.get(&path) {
+                                        let _ = thread_handle.join();
+                                    } else {
+                                        println!("WAV file not found in the HashMap.");
+                                    }
+                                }
                                 _ => (),
                             }
                         }
@@ -106,8 +118,4 @@ fn main() {
             _ => (),
         }
     });
-
-
-
-
 }
