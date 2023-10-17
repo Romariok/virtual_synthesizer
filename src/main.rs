aba15794856a729df9237e2402e6d656637d1d57
Romariok <ronya-ko@yandex.ru>
2023-10-18 00:35:46 +0300

    repair multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 mod data;
 mod services;
+extern crate hound;
 extern crate rodio;
 
 use data::collection::BasicPiano;
+use hound::SampleFormat;
+use std::fs::File;
+use std::io::BufReader;
 use winit::{
-    event::{Event, VirtualKeyCode, ElementState},
-    event_loop::{EventLoop, ControlFlow},
+    event::{ElementState, Event, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-use std::{fs::File, sync::Arc};
-use std::io::BufReader;
 
+use std::collections::HashMap;
 use std::time::Duration;
 use std::vec::Vec;
-use std::thread;
-use std::collections::HashMap;
+use std::{sync::Arc, thread};
 
+use async_std::task;
 use rodio::OutputStream;
 use rodio::OutputStreamHandle;
 
-extern crate hound;
-
 fn virtual_keycode_to_string(key_code: VirtualKeyCode) -> String {
+    // TODO: Implement enum with letters instead of hardcoding
+    // try to use &str since it is more applicable
     match key_code {
         VirtualKeyCode::A => "A".to_string(),
         VirtualKeyCode::B => "B".to_string(),
@@ -36,7 +39,7 @@ fn virtual_keycode_to_string(key_code: VirtualKeyCode) -> String {
 
 fn play_wav_file(file_name: &str, handle: OutputStreamHandle) {
     let file_path = format!("resources/basic/{}", file_name);
-    let file = File::open(&file_path).expect("Failed to open file");
+    let file = File::open(file_path).expect("Failed to open file");
     let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
     sink.append(source);
@@ -45,38 +48,39 @@ fn play_wav_file(file_name: &str, handle: OutputStreamHandle) {
     sink.stop();
 }
 
+
 fn main() {
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut threads: HashMap<String, thread::JoinHandle<()>> = HashMap::new();
     let (stream, handle) = OutputStream::try_default().unwrap();
     let spec = hound::WavSpec {
         channels: 1,
         sample_rate: 44100,
         bits_per_sample: 16,
-        sample_format: hound::SampleFormat::Int,
+        sample_format: SampleFormat::Int,
     };
     let mut wav_files: Vec<String> = Vec::new();
+    let basic: BasicPiano = BasicPiano::init_basic();
 
-    // Create a basic piano
-    {
-        let basic: BasicPiano = BasicPiano::initBasic();
+    for (key, value) in basic.as_map() {
+        // TODO: This method may panic due to .unwrap(). Method desc even says so
 
-        for (key, value) in basic.as_map() {
-            let mut writer = hound::WavWriter::create(format!("resources/basic/{}.wav", key), spec).unwrap();
-            wav_files.push(format!("resources/basic/{}.wav", key));
+        let mut writer =
+            hound::WavWriter::create(format!("resources/basic/{}.wav", key), spec).unwrap();
+        wav_files.push(format!("resources/basic/{}.wav", key));
 
-            for sample in value {
-                writer.write_sample(sample).unwrap();
-            }
+        for sample in value {
+            writer.write_sample(sample).unwrap();
         }
     }
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let mut threads: HashMap<String, thread::JoinHandle<()>> = HashMap::new();
 
-    // Spawn a new thread for each file
     for file_name in &wav_files {
         let file_name_clone = file_name.to_string();
         let handle_clone = handle.clone();
+
         let thread = thread::spawn(move || {
             play_wav_file(&file_name_clone, handle_clone);
         });
@@ -84,38 +88,49 @@ fn main() {
         threads.insert(file_name.to_string(), thread);
     }
 
+    // Create an Arc (atomic reference counter) for thread-safe access to handle
+    let handle_arc = Arc::new(handle);
+
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
 
         match event {
             Event::WindowEvent {
-                event,
+                event: winit::event::WindowEvent::KeyboardInput { input, .. },
                 window_id,
-            } => match event {
-                winit::event::WindowEvent::KeyboardInput { input, .. } => {
-                    if input.state == ElementState::Pressed {
-                        if let Some(keycode) = input.virtual_keycode {
-                            println!("Key pressed: {:?}", keycode);
-                            match keycode {
-                                VirtualKeyCode::C | VirtualKeyCode::D | VirtualKeyCode::E
-                                | VirtualKeyCode::F | VirtualKeyCode::G | VirtualKeyCode::A
-                                | VirtualKeyCode::B => {
-                                    let path = format!("{}.wav", virtual_keycode_to_string(keycode));
-                                    if let Some(thread_handle) = threads.get(&path) {
-                                        let _ = thread_handle.join();
-                                    } else {
-                                        println!("WAV file not found in the HashMap.");
-                                    }
-                                }
-                                _ => (),
+            } => {
+                if let Some(keycode) = input.virtual_keycode {
+                    if let ElementState::Pressed = input.state {
+                        println!("Key pressed: {:?}", keycode);
+                        match keycode {
+                            VirtualKeyCode::C
+                            | VirtualKeyCode::D
+                            | VirtualKeyCode::E
+                            | VirtualKeyCode::F
+                            | VirtualKeyCode::G
+                            | VirtualKeyCode::A
+                            | VirtualKeyCode::B => {
+                                // TODO: To use thread-safe values, use Arc;
+
+                                let handle_clone = Arc::clone(&handle_arc);
+                                let keycode = keycode;
+                                task::spawn(async move {
+                                    play_sound(handle_clone, &keycode).await;
+                                });
                             }
+                            _ => (),
                         }
                     }
                 }
-                _ => (),
-            },
-            Event::LoopDestroyed => return,
+            }
+            Event::LoopDestroyed => (),
             _ => (),
         }
     });
+}
+
+/// TODO: Build your logic
+async fn play_sound(handle: Arc<OutputStreamHandle>, keycode: &VirtualKeyCode) {
+
+    todo!()
 }
